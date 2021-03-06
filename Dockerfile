48191e15713e3b0dcc88c8972e3d5e47649525a4
snuzi <sabrinuzi@gmail.com>
2021-03-06 12:37:17 +0100

    Create index command should update index settings

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM php:7.4-fpm
 
 RUN apt-get update && apt-get install -y cron
 WORKDIR /var/eng-blogs
-COPY docker/cronjobs /etc/crontabs/root
--- src/Commands/CreateIndex.php
+++ src/Commands/CreateIndex.php
@@ -6,6 +6,7 @@ use EngBlogs\MeiliSearch\MeiliSearch;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class CreateIndex extends Command {
 
@@ -16,11 +17,15 @@ class CreateIndex extends Command {
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $meiliClient = new MeiliSearch(getenv('MEILI_INDEX_NAME'));
-        $meiliClient->createIndex();
-        $output->writeln('Index created');
+        
         try {
             $meiliClient->createIndex();
-            $output->writeln('Index created');
+            $output->writeln('Index created'); 
+
+            // Update index settings
+            $command = $this->getApplication()->find('db:update-index-settings');
+            $updateIndexSettingsInput = new ArrayInput([]);
+            $returnCode = $command->run($updateIndexSettingsInput, $output);
         } catch (\Exception $exception) {
             $output->writeln('Index already exists');
             return Command::FAILURE;
