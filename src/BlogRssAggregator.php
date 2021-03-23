e9d3ffe36afc28fbde2c5a9468eb2382cfe5308f
snuzi <sabrinuzi@gmail.com>
2021-03-23 19:06:13 +0100

    Handle blog rss feed fetching errors

--- src/BlogRssAggregator.php
+++ src/BlogRssAggregator.php
@@ -9,24 +9,31 @@ class BlogRssAggregator {
     public function run($rssFeed) {
         $meiliClient = new MeiliSearch(getenv('MEILI_INDEX_NAME'));
 
-        $blog = new Blog();
-        $blog->setName($rssFeed['title'])
-            ->setLink($rssFeed['blogUrl'])
-            ->setType($rssFeed['type'])
-            ->setGithubUsername($rssFeed['githubUsername'])
-            ->setRssFeed($rssFeed['rssFeed']);
+        try {
+            $blog = new Blog();
+            $blog->setName($rssFeed['title'])
+                ->setLink($rssFeed['blogUrl'])
+                ->setType($rssFeed['type'])
+                ->setGithubUsername($rssFeed['githubUsername'])
+                ->setRssFeed($rssFeed['rssFeed']);
 
-        if (!empty($rssFeed['image'])) {
-            $blog->setImage($rssFeed['image']);
+            if (!empty($rssFeed['image'])) {
+                $blog->setImage($rssFeed['image']);
+            }
+
+            printf("Aggregating %s ...\n", $blog->getName());
+
+            $feed = Reader::import($blog->getRssFeed());
+
+            $xmlParser = new FeedParser($feed, $blog, $meiliClient);
+            $posts = $xmlParser->getPosts();
+
+            $meiliClient->addDocuments($posts);
+            printf("%d documents indexed \n", count($posts));
+
+        } catch (\Exception $e) {
+            var_dump($e);
+            printf("Failed, message: \n", $e->getMessage());
         }
-
-        $feed = Reader::import($blog->getRssFeed());
-
-        $xmlParser = new FeedParser($feed, $blog, $meiliClient);
-        $posts = $xmlParser->getPosts();
-
-        printf("Crawling %s, %d docs to be indexed \n", $blog->getName(), count($posts));
-
-        $meiliClient->addDocuments($posts);
     }
 }
